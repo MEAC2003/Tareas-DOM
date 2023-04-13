e6df3c9a7c0657feab7d638803ffebe6d8b6e557
MEAC <manuelantoncisneros2020@hotmail.com>
2023-04-13 01:02:07 -0500

    Clase 05 completada

--- script.js
+++ script.js
@@ -1,50 +1,35 @@
-(() => {
-  const btn = document.querySelector("[data-form-btn]");
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
-  //Arrow function o funciones anonimas
-  const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    if (value != "") {
-      task.classList.add("card");
-      input.value = "";
-      //backticks
-      const taskContent = document.createElement("div");
-      const titleTask = document.createElement("span");
-      titleTask.classList.add("task");
-      titleTask.innerText = value;
-      taskContent.appendChild(checkComplete());
-      taskContent.appendChild(titleTask);
-      const content =
-        `<i class="fas fa-trash-alt trashIcon icon"></i>`
-      // task.innerHTML = content;
-      task.appendChild(taskContent)
-      list.appendChild(task);
-    } else {
-      alert("Por favor, debe agregar contenido");
-    }
-  };
+const btn = document.querySelector("[data-form-btn]");
 
-  btn.addEventListener("click", createTask);
-
-  const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add("far", "fa-check-square", "icon");
-    i.addEventListener("click", completeTask)
-    return i;
+//Arrow function o funciones anonimas
+const createTask = (evento) => {
+  evento.preventDefault();
+  const input = document.querySelector("[data-form-input]");
+  const value = input.value;
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  if (value != "") {
+    task.classList.add("card");
+    input.value = "";
+    //backticks
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    // task.innerHTML = content;
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
+    list.appendChild(task);
+  } else {
+    alert("Por favor, debe agregar contenido");
   }
-  // Immediately invoked function expression IIFE
-  const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-  }
+};
 
-})();
+btn.addEventListener("click", createTask);
 
 // Sin Arrow function
 // btn.addEventListener("click", function (evento) {
